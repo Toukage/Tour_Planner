a334ec65b219d3b7b11a656cb60ecc4ea7536805
Toukage <if22b087@technikum-wien.at>
2025-02-24 11:42:56 +0100

    Updated APP to have a main , and the MainWindow to only have a create and delete button so than we can add the other components later

---
+++ src/View/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TourPlanner.View
+{
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+
+}
