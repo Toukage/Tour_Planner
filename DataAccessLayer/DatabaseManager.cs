baa6545ff42d95917d21aa1b39433fdeeffbf103
Toukage <if22b087@technikum-wien.at>
2025-02-24 11:20:39 +0100

    Updated BL and DAL to be Class Libraries

---
+++ DataAccessLayer/DatabaseManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TourPlanner.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.DataAccessLayer
+{
+
+
+    namespace TourPlanner.DAL
+    {
+        public class DatabaseManager : DbContext
+        {
+            public DbSet<Tour> Tours { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Username=toukage;Password=tourplanner;Database=TP_DB");
+            }
+        }
+    }
+
+}
