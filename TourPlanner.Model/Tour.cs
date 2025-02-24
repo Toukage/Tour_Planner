f7c10921fe336bd0d5b28109a861b49d7103321e
Toukage <if22b087@technikum-wien.at>
2025-02-24 11:21:46 +0100

    Changed Model to Class Library, temporary fix as each model will be moved into its respective class library in the future

---
+++ TourPlanner.Model/Tour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourPlanner.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace TourPlanner.Model
+{
+    [Table("Tour")]
+    public class Tour
+    {
+        [Key]
+        public int TourID { get; set; }
+
+        [Required, StringLength(225)]
+        public string TourName { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required, StringLength(50)]
+        public string TourStart { get; set; }
+
+        [Required, StringLength(50)]
+        public string TourEnd { get; set; }
+
+        [Required, StringLength(50)]
+        public string Transport { get; set; }
+
+        public float? Distance { get; set; }
+        public float? EstTime { get; set; }
+        public string RouteMap { get; set; }
+    }
+}
