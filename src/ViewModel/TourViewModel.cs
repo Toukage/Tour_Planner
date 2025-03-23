2c6352da30e97f33e5847c26fa8acabf01612b94
electric sheep <if22b094@technikum-wien.at>
2025-03-24 00:00:54 +0100

    Set up event handlers , connects UI to BL with relay commands

---
+++ src/ViewModel/TourViewModel.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer;
+using log4net.Config;
+using System.ComponentModel;
+using System.Windows.Input;
+using TourPlanner.Model;
+
+namespace TourPlanner.ViewModel
+{
+    public class TourViewModel : INotifyPropertyChanged
+    {
+        private readonly TourLogic _tourLogic;
+        private Tour _tour;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TourViewModel()
+        {
+            _tourLogic = new TourLogic();
+            Tour = new Tour();
+            XmlConfigurator.Configure();
+            CreateTourCommand = new Relay(CreateTour);
+            DeleteTourCommand = new Relay(DeleteTour);
+        }
+
+        public Tour Tour
+        {
+            get => _tour;
+            set { _tour = value; OnPropertyChanged(nameof(Tour)); }
+        }
+
+        public ICommand CreateTourCommand { get; private set; }
+        public ICommand DeleteTourCommand { get; private set; }
+
+
+        private void CreateTour()
+        {
+            _tourLogic.CreateNewTour(Tour);
+        }
+
+        private void DeleteTour()
+        {
+            _tourLogic.DeleteTour(Tour);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
