ac7259bf853d49ee4054fa0eba2edb3330f83fa0
electric sheep <if22b094@technikum-wien.at>
2025-03-24 00:03:14 +0100

    relay commands added

---
+++ src/Relay.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+
+namespace TourPlanner
+{
+    public class Relay : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public Relay(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute();
+        public void Execute(object parameter) => _execute();
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
