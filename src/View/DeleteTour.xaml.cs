e6e5e24bcb8f666150184c3c09d936a93758a1b6
electric sheep <if22b094@technikum-wien.at>
2025-03-23 23:56:51 +0100

    set up tour deletion window

---
+++ src/View/DeleteTour.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using TourPlanner.ViewModel;
+
+namespace TourPlanner.View
+{
+    public partial class DeleteTour : Window
+    {
+        public DeleteTour()
+        {
+            InitializeComponent();
+            this.DataContext = new TourViewModel();
+        }
+    }
+}
---
+++ src/View/DeleteTourGrid.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using TourPlanner.ViewModel;
+
+
+namespace TourPlanner.View
+{
+    public partial class CreateTourGrid : UserControl
+    {
+        public CreateTourGrid()
+        {
+            InitializeComponent();
+            this.DataContext = new TourViewModel();
+        }
+
+        private void SaveTourBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Window.GetWindow(this) is CreateTour createTourWindow)
+            {
+                createTourWindow.Close();
+            }
+        }
+    }
+}
