ea7630db898b6786a5ca99af75078d1d3ba95388
electric sheep <if22b094@technikum-wien.at>
2025-03-23 23:48:40 +0100

    set up class for binding and added button logic to close window after sending out the request

--- src/View/CreateTourGrid.xaml.cs
+++ src/View/CreateTourGrid.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using TourPlanner.ViewModel;
+
 
 namespace TourPlanner.View
 {
@@ -20,10 +11,15 @@ namespace TourPlanner.View
         public CreateTourGrid()
         {
             InitializeComponent();
+            this.DataContext = new TourViewModel();
         }
+
         private void SaveTourBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            if (Window.GetWindow(this) is CreateTour createTourWindow)
+            {
+                createTourWindow.Close();
+            }
         }
     }
 }
