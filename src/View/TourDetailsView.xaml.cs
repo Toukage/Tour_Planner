353a63296e1b375dfade35d9c7e2dcff3c6fdc21
Toukage <if22b087@technikum-wien.at>
2025-09-18 08:58:44 +0200

    added a detailed view for tours

---
+++ src/View/TourDetailsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace TourPlanner.View
+{
+    public partial class TourDetailsView : UserControl
+    {
+        public TourDetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
