4ac6144eab9077434df48de03eb578b13255978a
Toukage <if22b087@technikum-wien.at>
2025-09-18 13:54:35 +0200

    added service collection

--- src/View/App.xaml.cs
+++ src/View/App.xaml.cs
@@ -1,8 +1,11 @@
-﻿using log4net;
+﻿using BusinessLayer;
+using log4net;
 using log4net.Config;
-using System.Configuration;
-using System.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.IO;
+using System.Net.Http;
 using System.Reflection;
 using System.Windows;
 
@@ -11,30 +14,123 @@ namespace TourPlanner.View
     public partial class App : Application
     {
         private static readonly ILog log = LogManager.GetLogger(typeof(App));
+        private ServiceProvider? _services;
 
         [STAThread]
         public static void Main()
         {
-            // 1) BaseDir = Ordner der laufenden EXE (bin\Debug\...\)
-            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
-
-            // 2) logs-Unterordner erzeugen
-            var logsDir = Path.Combine(baseDir, "logs");
-            Directory.CreateDirectory(logsDir);
-
-            // 3) log4net-Property setzen, die in der XML verwendet wird
-            log4net.GlobalContext.Properties["LogDir"] = logsDir;
-
-            // 4) log4net.config laden
-            var logConfigFile = Path.Combine(baseDir, "log4net.config");
-            XmlConfigurator.Configure(LogManager.GetRepository(Assembly.GetEntryAssembly()),
-                                      new FileInfo(logConfigFile));
-
-            log.Info("----------------> NEW LOG START <----------------");
-
+            InitLogging();
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            this.DispatcherUnhandledException += (_, args) =>
+            {
+                log.Error("UI thread exception", args.Exception);
+                args.Handled = true;
+            };
+            AppDomain.CurrentDomain.UnhandledException += (_, args2) =>
+            {
+                log.Fatal("Non-UI thread exception", args2.ExceptionObject as Exception);
+            };
+            TaskScheduler.UnobservedTaskException += (_, args3) =>
+            {
+                log.Error("Unobserved task exception", args3.Exception);
+                args3.SetObserved();
+            };
+
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            _services = services.BuildServiceProvider();
+
+            log.Info("----------------> NEW LOG START <----------------");
+            var mainWindow = _services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            log.Info("Application exiting.");
+            _services?.Dispose();
+            log4net.LogManager.Shutdown();
+            base.OnExit(e);
+        }
+
+        private static void InitLogging()
+        {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var logsDir = Path.Combine(baseDir, "Logging");
+            Directory.CreateDirectory(logsDir);
+            log4net.GlobalContext.Properties["LogDir"] = logsDir;
+
+            var logConfigFile = Path.Combine(baseDir, "log4net.config");
+            XmlConfigurator.Configure(LogManager.GetRepository(Assembly.GetEntryAssembly()), new FileInfo(logConfigFile));
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            //Config
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var orsApiKey = config["OpenRouteService:ApiKey"]
+               ?? Environment.GetEnvironmentVariable("ORS_API_KEY")
+               ?? "";
+
+            if (string.IsNullOrWhiteSpace(orsApiKey))
+            {
+                MessageBox.Show("Kein OpenRouteService API Key gefunden!", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw new InvalidOperationException("OpenRouteService API key fehlt!");
+            }
+
+            services.AddSingleton(sp => new GeoCode(sp.GetRequiredService<HttpClient>(), orsApiKey));
+            services.AddSingleton(sp => new Directions(sp.GetRequiredService<HttpClient>(), orsApiKey));
+
+            //DAL
+            var connString = config.GetConnectionString("Default");
+            services.AddSingleton<IConfiguration>(config);
+
+            services.AddDbContextFactory<DataAccessLayer.DatabaseManager>(opt =>
+            {
+                opt.UseNpgsql(connString);
+            });
+
+            services.AddSingleton(new HttpClient { Timeout = TimeSpan.FromSeconds(20) });
+            services.AddSingleton<DataAccessLayer.TourRepo>();
+            services.AddSingleton<DataAccessLayer.LogRepo>();
+
+            //BL
+            services.AddSingleton<Routing>();
+            services.AddSingleton<TourLogic>();
+            services.AddSingleton<LogLogic>();
+            services.AddSingleton<Report>();
+
+            //View Models
+            services.AddSingleton<TourPlanner.ViewModel.TourListVM>();
+            services.AddSingleton<TourPlanner.ViewModel.LogListVM>();
+            services.AddSingleton<TourPlanner.ViewModel.TourDetailsVM>();
+            services.AddSingleton<TourPlanner.ViewModel.MapVM>();
+            services.AddSingleton<TourPlanner.ViewModel.MainVM>();
+            services.AddTransient<TourPlanner.ViewModel.CreateTourVM>();
+            services.AddTransient<TourPlanner.ViewModel.CreateLogVM>();
+            services.AddTransient <TourPlanner.ViewModel.ModifyTourVM>();
+            services.AddTransient<TourPlanner.ViewModel.ModifyLogVM>();
+            //Views
+            services.AddTransient<TourPlanner.View.MainWindow>();
+            services.AddTransient<TourPlanner.View.CreateTour>(); 
+            services.AddTransient<TourPlanner.View.CreateTourLog>();
+            services.AddTransient<TourPlanner.View.ModifyTour>();
+            services.AddTransient<TourPlanner.View.ModifyLog>();
+            services.AddTransient<TourPlanner.View.LogListView>();
+            services.AddTransient<TourPlanner.View.TourListView>();
+            services.AddTransient<TourPlanner.View.TourDetailsView>();
         }
     }
 }
