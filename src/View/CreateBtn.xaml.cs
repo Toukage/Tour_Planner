9d82e1096e01352855b0f70908e98cd1d81dae96
Toukage <if22b087@technikum-wien.at>
2025-09-18 13:48:06 +0200

    added reusable btns for creating, modifying and deleting

---
+++ src/View/CreateBtn.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TourPlanner.View
+{
+    public partial class CreateBtn : UserControl
+    {
+        public CreateBtn() => InitializeComponent();
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(CreateBtn));
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register(nameof(CommandParameter), typeof(object), typeof(CreateBtn));
+
+        public static readonly DependencyProperty IsButtonEnabledProperty =
+            DependencyProperty.Register(nameof(IsButtonEnabled), typeof(bool), typeof(CreateBtn), new PropertyMetadata(true));
+
+        public ICommand Command { get => (ICommand)GetValue(CommandProperty); set => SetValue(CommandProperty, value); }
+        public object CommandParameter { get => GetValue(CommandParameterProperty); set => SetValue(CommandParameterProperty, value); }
+        public bool IsButtonEnabled { get => (bool)GetValue(IsButtonEnabledProperty); set => SetValue(IsButtonEnabledProperty, value); }
+    }
+}
---
+++ src/View/DeleteBtn.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TourPlanner.View
+{
+    public partial class DeleteBtn : UserControl
+    {
+        public DeleteBtn() => InitializeComponent();
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(DeleteBtn));
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register(nameof(CommandParameter), typeof(object), typeof(DeleteBtn));
+
+        public static readonly DependencyProperty IsButtonEnabledProperty =
+            DependencyProperty.Register(nameof(IsButtonEnabled), typeof(bool), typeof(DeleteBtn), new PropertyMetadata(true));
+
+        public ICommand Command { get => (ICommand)GetValue(CommandProperty); set => SetValue(CommandProperty, value); }
+        public object CommandParameter { get => GetValue(CommandParameterProperty); set => SetValue(CommandParameterProperty, value); }
+        public bool IsButtonEnabled { get => (bool)GetValue(IsButtonEnabledProperty); set => SetValue(IsButtonEnabledProperty, value); }
+    }
+}
