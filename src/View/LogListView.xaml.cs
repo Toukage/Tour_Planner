fc537494d468e2a1afb08adaf01695de52e78bea
Toukage <if22b087@technikum-wien.at>
2025-09-18 13:19:45 +0200

    added a listview for logs

---
+++ src/View/LogListView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace TourPlanner.View
+{
+    public partial class LogListView : UserControl
+    {
+        public LogListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
